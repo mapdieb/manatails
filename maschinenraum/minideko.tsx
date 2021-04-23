32a57527b761d5846c8dabee39db1e8cd5a0ff3b
mapdieb <83021616+mapdieb@users.noreply.github.com>
2021-04-23 16:17:16 +0200

    Add files via upload

---
+++ maschinenraum/minideko.tsx
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="minideko" tilewidth="32" tileheight="32" tilecount="91" columns="7">
+ <image source="minideko.png" trans="ff00ff" width="224" height="416"/>
+ <tile id="14">
+  <objectgroup draworder="index">
+   <object id="1" x="2.04147" y="17.3525">
+    <polygon points="0,0 21.8607,-0.425306 20.6699,-14.3753 -0.340245,-14.5455"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="8.93142" y="13.0144">
+    <polygon points="0,0 1.70122,-9.95215 21.6906,-10.6326 21.6055,2.89208"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <animation>
+   <frame tileid="39" duration="5000"/>
+   <frame tileid="33" duration="500"/>
+   <frame tileid="39" duration="10000"/>
+   <frame tileid="32" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="48">
+  <animation>
+   <frame tileid="36" duration="140"/>
+   <frame tileid="36" duration="140"/>
+   <frame tileid="36" duration="140"/>
+   <frame tileid="36" duration="140"/>
+   <frame tileid="48" duration="140"/>
+  </animation>
+ </tile>
+</tileset>
