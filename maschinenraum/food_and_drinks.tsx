b0379f93baeb805a4b3bd6d346a57c9a0bafaef4
mapdieb <83021616+mapdieb@users.noreply.github.com>
2021-04-23 17:52:53 +0200

    Add files via upload

---
+++ maschinenraum/food_and_drinks.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="food_and_drinks" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
+ <image source="food_and_drinks.png" trans="ff00ff" width="512" height="2048"/>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="7.31526" y="20.925">
+    <polygon points="0,0 24.1574,0.170122 24.4125,10.5476 -0.170122,10.8878"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index">
+   <object id="1" x="7.06007" y="0.340245">
+    <polygon points="0,0 0,30.7921 24.7528,31.0473 24.5827,-0.170122"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index">
+   <object id="1" x="6.63477" y="0.510367">
+    <polygon points="0,0 0.510367,20.4997 25.5183,20.4147 25.093,-0.340245"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
