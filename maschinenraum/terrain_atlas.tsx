378a15d17f9e5eb3eb1f16c5a844faf56ff55a80
mapdieb <83021616+mapdieb@users.noreply.github.com>
2021-04-23 18:00:30 +0200

    Delete maschinenraum directory

--- maschinenraum/terrain_atlas.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="terrain_atlas" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="terrain_atlas.png" trans="ff00ff" width="1024" height="1024"/>
-</tileset>
