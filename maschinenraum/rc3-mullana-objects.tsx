ab551c610dc346ee355e4dcaa8069cc48757dff3
mapdieb <83021616+mapdieb@users.noreply.github.com>
2021-04-23 18:01:18 +0200

    Add files via upload

---
+++ maschinenraum/rc3-mullana-objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="rc3-mullana-objects" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+ <image source="rc3-mullana-objects.png" trans="ff00ff" width="512" height="256"/>
+</tileset>
