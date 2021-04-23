47843e7997292a34ba2e9d0fee8f4822b73d0964
mapdieb <83021616+mapdieb@users.noreply.github.com>
2021-04-23 17:52:08 +0200

    Delete maschinenraum directory

--- maschinenraum/01_Tile_Set_HarryPotter_Lichter.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="01_Tile_Set_HarryPotter_Lichter" tilewidth="32" tileheight="32" tilecount="648" columns="18">
- <image source="01_Tile_Set_HarryPotter_Lichter.png" width="576" height="1152"/>
-</tileset>
