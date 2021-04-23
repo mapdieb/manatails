2bb54f1d44cac4d1f7eac4c793b56c6fb0687d54
mapdieb <83021616+mapdieb@users.noreply.github.com>
2021-04-23 18:13:41 +0200

    Add files via upload

---
+++ maschinenraum/plums.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="plums" tilewidth="32" tileheight="32" tilecount="475" columns="25">
+ <image source="plums.png" trans="ff00ff" width="800" height="608"/>
+ <tile id="194">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
